62e076b4787fe4d8c7c3da095b186f3fbdeef2b1
Naina Vaskenly <quic_nvaskenl@quicinc.com>
2022-11-18 16:48:50 +0530

    qssi_au: qssi_au.mk: include ais-sys product.mk
    Include ais-sys product.mk in qssi_au.mk

--- qssi_au.mk
+++ qssi_au.mk
@@ -204,6 +204,8 @@ PRODUCT_PACKAGES += android.hardware.camera.provider@2.4-impl
 # Enable binderized camera HAL
 PRODUCT_PACKAGES += android.hardware.camera.provider@2.4-service_64
 
+$(call inherit-product-if-exists, $(TOPDIR)vendor/qcom/proprietary/commonsys/ais-sys/product.mk)
+
 # Ethernet configuration file
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml
