1fabe8117438488b1d2f3071a54b8b190d76970d
Asmeer Mohammed <quic_asmemoha@quicinc.com>
2023-03-01 16:14:51 +0530

    qssi_au: Disabled vbmeta_system
    Enabling vbmeta_system will cause boot failure in GVM as its
    partition is not enabled for GVM platforms
    
    Change-Id: I1d6bd6633841b9e86acf937a4cab8d1722cdcf7d
    Signed-off-by: Asmeer Mohammed <quic_asmemoha@quicinc.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -152,7 +152,7 @@ BOARD_QTI_DYNAMIC_PARTITIONS_PARTITION_LIST := system system_ext system_dlkm
 BOARD_QTI_DYNAMIC_PARTITIONS_SIZE := 5314772992
 BOARD_EXT4_SHARE_DUP_BLOCKS := true
 ifeq ($(ENABLE_AB), true)
-AB_OTA_PARTITIONS ?= system system_ext vbmeta_system system_dlkm
+AB_OTA_PARTITIONS ?= system system_ext system_dlkm
 endif
 endif
 ###### Dynamic Partition Handling ####
