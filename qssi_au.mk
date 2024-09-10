314989a944fd8c06be6c53cfa9de27664ed01d74
Vijayakumar Badiger <quic_vbadiger@quicinc.com>
2024-09-10 09:46:05 -0700

    qssi_au: add avbctl tool read the verified boot status
    Add avbctl tool for reading the avb2.0 status on the
    target.
    
    Change-Id: I81bb97e4aff7c3598ccbf40c4458473cc0395594
    Signed-off-by: Vijayakumar Badiger <quic_vbadiger@quicinc.com>
    (cherry picked from commit 87eed1a1242c5b3a1b1da8cfae353b6f332f542d)

--- qssi_au.mk
+++ qssi_au.mk
@@ -140,6 +140,8 @@ BOARD_FRP_PARTITION_NAME := frp
 #Android EGL implementation
 PRODUCT_PACKAGES += libGLES_android
 
+PRODUCT_PACKAGES_DEBUG += avbctl
+
 ifneq ($(TARGET_NO_TELEPHONY), true)
 PRODUCT_BOOT_JARS += telephony-ext
 PRODUCT_PACKAGES += telephony-ext
