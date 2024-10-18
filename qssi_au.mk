e7c9a3290baf1efbfc1fd66dfbc6f438ac6e2078
meixie <quic_meixie@quicinc.com>
2024-10-18 11:17:07 +0800

    qssi_au: Add two scripts to create vlan interface
    Add two scritps to target to create vlan interface.
    
    Change-Id: I20d5769ee81350c7f2fb40766af8cd06e6fa6c5f
    Signed-off-by: meixie <quic_meixie@quicinc.com>

--- qssi_au.mk
+++ qssi_au.mk
@@ -350,6 +350,9 @@ endif
 
 PRODUCT_PACKAGES += vendor.qti.qesdsys
 
+PRODUCT_PACKAGES += create_vlan_vm_1.sh \
+                    create_vlan_vm_2.sh
+
 # Enable support for APEX updates
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
