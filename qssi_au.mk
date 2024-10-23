f2336ed0e117f4a74721951285ef66646b7c714f
Mrinmoy Samanta <quic_samanta@quicinc.com>
2024-10-23 04:47:56 -0700

    Add compatibility matrix for someip stack agent.
    Change-Id: I5c6debfdd0668b258f8312d16ed5b4aa0280f71e

--- qssi_au.mk
+++ qssi_au.mk
@@ -214,6 +214,10 @@ PRODUCT_PACKAGES += \
 
 DEVICE_FRAMEWORK_MANIFEST_FILE := device/qcom/qssi_au/framework_manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := vendor/qcom/opensource/core-utils/vendor_framework_compatibility_matrix.xml
+ifeq ($(TARGET_AUTO_RBVM), true)
+    DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+                                                  device/qcom/qssi_au/compatibility_matrix.xml
+endif
 
 PRODUCT_PRODUCT_PROPERTIES += persist.adb.tcp.port=5555
 
