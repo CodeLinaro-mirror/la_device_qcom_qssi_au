572f83f42c6d4e61eb0a5c2815b7ff773a238a2c
meixie <quic_meixie@quicinc.com>
2025-04-09 18:13:18 +0800

    qssi_au: add one script to monitor link status
    Added one script to monitor link status, so we are able to add TC
    filter back once link is up.
    
    Change-Id: Ie3e605143fb8769aad0b9c2d14d65135b9a69ad7
    Signed-off-by: meixie <quic_meixie@quicinc.com>

--- qssi_au.mk
+++ qssi_au.mk
@@ -382,6 +382,7 @@ PRODUCT_PACKAGES += vendor.qti.qesdsys
 PRODUCT_PACKAGES += create_vlan_vm_1.sh \
                     create_vlan_vm_2.sh \
                     eth_cli.sh \
+                    link_monitor.sh \
 
 # Enable support for APEX updates
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
