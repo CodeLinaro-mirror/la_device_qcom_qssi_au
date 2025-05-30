94dfb80c8f512b731a4f91117044a5220db6e2aa
quic_deepaky <quic_deepaky@quicinc.com>
2025-05-30 20:24:57 +0530

    Add MultiDisplayTest and MultiDisplaySecondaryHomeTestLauncher
    Added support for Secondary Home Launcher
    
    Change-Id: I504c9ef54d5a2e01cb9817881c6b1378e03c4b94
    Signed-off-by: quic_deepaky <quic_deepaky@quicinc.com>

--- qssi_au.mk
+++ qssi_au.mk
@@ -280,6 +280,9 @@ PRODUCT_PACKAGES += hsi2s_test
 # enables the rro package for passenger(secondary) user.
 ENABLE_PASSENGER_SYSTEMUI_RRO := true
 
+PRODUCT_PACKAGES += MultiDisplaySecondaryHomeTestLauncher \
+                    MultiDisplayTest
+
 # Kernel modules install path
 KERNEL_MODULES_INSTALL := dlkm
 KERNEL_MODULES_OUT := out/target/product/$(PRODUCT_NAME)/$(KERNEL_MODULES_INSTALL)/lib/modules
