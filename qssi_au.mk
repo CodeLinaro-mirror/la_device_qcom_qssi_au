b745f65bb282e131d997c595c72c2afa03c47c7a
Deepak Yadav <deepaky@qti.qualcomm.com>
2025-06-04 02:59:12 -0700

    Revert "[8255_HQX4570] Add packages for MUMD enablement"
    This reverts commit 92f94e51426d7378d31815a4539b727bb8323f7e.
    
    Reason for revert: System instable: Possible Google Bug
    
    Change-Id: I026c5ac6798de7ddae2b681cdacb337b529c86a6

--- qssi_au.mk
+++ qssi_au.mk
@@ -278,9 +278,6 @@ PRODUCT_PACKAGES_DEBUG += ethtool \
 # HS-I2S test app
 PRODUCT_PACKAGES += hsi2s_test
 
-# enables the rro package for passenger(secondary) user.
-ENABLE_PASSENGER_SYSTEMUI_RRO := true
-
 # Kernel modules install path
 KERNEL_MODULES_INSTALL := dlkm
 KERNEL_MODULES_OUT := out/target/product/$(PRODUCT_NAME)/$(KERNEL_MODULES_INSTALL)/lib/modules
