55b96f54181a102e20d0b6a236f14ad7a04c5d5a
Sindhu Kanathur <quic_skanathu@quicinc.com>
2025-07-22 17:10:33 +0530

    qssi_au: Disable GAS for Android 16
    Change-Id: I71ab3c6d3377c48ccca2f67d3d8f62900d56155e
    Signed-off-by: Sindhu Kanathur <quic_skanathu@quicinc.com>
    (cherry picked from commit 9d78cf441bbd584198ee00449dcc04d38dc1c40b)

--- qssi_au.mk
+++ qssi_au.mk
@@ -95,7 +95,7 @@ TARGET_USES_QSSI := true
 
 TARGET_USES_NEW_ION := true
 
-TARGET_USES_GAS := true
+TARGET_USES_GAS := false
 
 ifeq ($(strip $(TARGET_BUILD_VARIANT)),user)
 ifeq ($(TARGET_USES_GAS),true)
