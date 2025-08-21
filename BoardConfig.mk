b59b56c7bad68095425b7d77b731fe7e23822ce5
Houffaneh Osman <hosman@qti.qualcomm.com>
2025-08-21 12:23:30 +0000

    Revert "qssi_au: Disabled vbmeta_system"
    Required for COQOS Android GVM
    This reverts commit fbbfa8b194f39bec7f985f9d8c5c98f636611c61.
    
    Change-Id: I6f644241ff6a7d6c96bdd199a594cd0f40663038

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -159,7 +159,7 @@ BOARD_QTI_DYNAMIC_PARTITIONS_PARTITION_LIST := system system_ext product
 BOARD_QTI_DYNAMIC_PARTITIONS_SIZE := 5314772992
 BOARD_EXT4_SHARE_DUP_BLOCKS := true
 ifeq ($(ENABLE_AB), true)
-AB_OTA_PARTITIONS ?= system system_ext product
+AB_OTA_PARTITIONS ?= system system_ext vbmeta_system product
 endif
 endif
 ###### Dynamic Partition Handling ####
