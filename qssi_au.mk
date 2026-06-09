e2588f90750c661b068b12b1d67c1782069010d9
Rohit Nehra <rnehra@qti.qualcomm.com>
2026-06-09 12:44:45 +0530

    qssi_au : removing properties from system.prop
    Move persist.bluetooth.enablenewavrcp, config.disable_rtt, and
    persist.debug.wfd.enable from system.prop to PRODUCT_SYSTEM_PROPERTIES
    
    Change-Id: I11bac1a642d273f2bed1cb52b5da3018e4e25d76
    Signed-off-by: Rohit Nehra <rnehra@qti.qualcomm.com>
    (cherry picked from commit ef5bb43ec23b2fe2cacf5dec3949edabf2a92786)

--- qssi_au.mk
+++ qssi_au.mk
@@ -242,6 +242,16 @@ PRODUCT_SYSTEM_PROPERTIES += \
 
 PRODUCT_SYSTEM_PROPERTIES += ro.android.car.audio.enableaudiopatch=true
 
+# The property "persist.bluetooth.enablenewavrcp" is introduced in AOSP.
+# In Automotive SP, AVRCP(CT) is enabled in Car UI, so set to false.
+PRODUCT_SYSTEM_PROPERTIES += persist.bluetooth.enablenewavrcp=false
+
+# Disable RTT (Real-Time Text) feature
+PRODUCT_SYSTEM_PROPERTIES += config.disable_rtt=true
+
+# Enable WFD debug property
+PRODUCT_SYSTEM_PROPERTIES += persist.debug.wfd.enable=1
+
 # USB default HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
